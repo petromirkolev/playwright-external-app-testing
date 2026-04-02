ff76eafb9337c9b8dbf9aaa08596574ef76f784d
Petromir Kolev <petromirkolev@gmail.com>
2026-04-02 07:28:57 +0300

    wip: Save progress

--- contact-list/tests/api/auth.spec.ts
+++ contact-list/tests/api/auth.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '../../fixtures/api';
 import { api } from '../../utils/api-helpers';
-import { uniqueEmail } from '../../utils/test-data';
+import { INVALID_EMAIL } from '../../utils/constants';
+import { invalidContactInputs, uniqueEmail } from '../../utils/test-data';
 
 test.describe('Registration', () => {
-  test('User registration is successful', async ({
+  test('Registration with valid credentials is successful', async ({
     request,
     registrationData,
   }) => {
@@ -47,9 +48,85 @@ test.describe('Registration', () => {
     expect(duplicateResponse.status()).toBe(400);
     expect(duplicateBody.message).toBe('Email address is already in use');
   });
+
+  test('Registration with invalid email is rejected', async ({
+    request,
+    registrationData,
+  }) => {
+    const email = invalidContactInputs.email;
+
+    const response = await api.register(request, {
+      ...registrationData,
+      email,
+    });
+
+    const body = await response.json();
+
+    expect(response.status()).toBe(400);
+    expect(body.message).toBe(INVALID_EMAIL);
+  });
+
+  test('Registration with invalid passwrod too short is rejected', async ({
+    request,
+    registrationData,
+  }) => {
+    const email = uniqueEmail();
+
+    const response = await api.register(request, {
+      ...registrationData,
+      email,
+    });
+
+    expect(response.status()).toBe(201);
+
+    const duplicateResponse = await api.register(request, {
+      ...registrationData,
+      email,
+    });
+
+    const duplicateBody = await duplicateResponse.json();
+
+    expect(duplicateResponse.status()).toBe(400);
+    expect(duplicateBody.message).toBe('Email address is already in use');
+  });
+
+  test('Registration with invalid password too long rejected', async ({
+    request,
+    registrationData,
+  }) => {
+    const email = uniqueEmail();
+
+    const response = await api.register(request, {
+      ...registrationData,
+      email,
+    });
+
+    expect(response.status()).toBe(201);
+
+    const duplicateResponse = await api.register(request, {
+      ...registrationData,
+      email,
+    });
+
+    const duplicateBody = await duplicateResponse.json();
+
+    expect(duplicateResponse.status()).toBe(400);
+    expect(duplicateBody.message).toBe('Email address is already in use');
+  });
 });
 
-// register duplicate user reject with expected error
+test.describe('Login', () => {
+  test('Login with registered credentials is successful', async () => {});
+
+  test('Login with invalid email is rejected', async () => {});
+
+  test('Login with invalid password is rejected', async () => {});
+
+  test('Login with missing required email is rejected', async () => {});
+
+  test('Login with missing required password is rejected', async () => {});
+});
+
 // login valid user > success > token/user payload
 // login invalid password > reject
 // login missing required fields > reject
---
+++ contact-list/utils/constants.ts
@@ -0,0 +1,1 @@
+export const INVALID_EMAIL = 'User validation failed: email: Email is invalid';
