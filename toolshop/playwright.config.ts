afcbb0a333367a69c46c0cd02de839320f78a901
Petromir Kolev <petromirkolev@gmail.com>
2026-04-08 17:14:47 +0300

    feat: Add Allure reporting

--- toolshop/playwright.config.ts
+++ toolshop/playwright.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   timeout: 30_000,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 3 : undefined,
-  reporter: [['html']],
+  reporter: [
+    ['html'],
+    ['allure-playwright', { outputFolder: 'allure-results' }],
+  ],
   use: {
     baseURL: process.env.BASE_URL || 'https://api.practicesoftwaretesting.com/',
     headless: true,
