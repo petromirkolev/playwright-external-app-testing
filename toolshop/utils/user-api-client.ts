28eae3e128900feb185d850f5d5c98e920f19030
Petromir Kolev <petromirkolev@gmail.com>
2026-04-09 14:56:53 +0300

    chore: Rename and update user-api-client file

---
+++ toolshop/utils/user-api-client.ts
@@ -0,0 +1,72 @@
+import { APIRequestContext, APIResponse } from '@playwright/test';
+import { LoginInput, RegistrationInput } from '../types/user';
+
+export class UserApiClient {
+  constructor(private readonly request: APIRequestContext) {}
+
+  async register(input: Partial<RegistrationInput>): Promise<APIResponse> {
+    return this.request.post('users/register', {
+      data: input,
+    });
+  }
+
+  async login(input: Partial<LoginInput>): Promise<APIResponse> {
+    return this.request.post('users/login', {
+      data: input,
+    });
+  }
+
+  async delete(
+    id: string | undefined,
+    token: string | undefined,
+  ): Promise<APIResponse> {
+    return this.request.delete(`users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+
+  async update(
+    input: Partial<RegistrationInput>,
+    id: string | undefined,
+    token: string | undefined,
+  ): Promise<APIResponse> {
+    return this.request.put(`users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+      data: input,
+    });
+  }
+
+  async partialUpdate(
+    input: Partial<RegistrationInput>,
+    id: string | undefined,
+    token: string | undefined,
+  ): Promise<APIResponse> {
+    return this.request.patch(`users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+      data: input,
+    });
+  }
+
+  async get(
+    id: string | undefined,
+    token: string | undefined,
+  ): Promise<APIResponse> {
+    return this.request.get(`users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+
+  async getMe(token: string | undefined): Promise<APIResponse> {
+    return this.request.get('users/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  }
+
+  async refreshToken(token: string | undefined | null): Promise<APIResponse> {
+    return this.request.get('users/refresh', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  }
+}
