9fc685d4d181ba456bc0bfe34b9a51b6e4bf5d95
Petromir Kolev <petromirkolev@gmail.com>
2026-04-13 09:42:51 +0300

    chore: Add valid product partial update data

--- toolshop/utils/product-data.ts
+++ toolshop/utils/product-data.ts
@@ -12,6 +12,94 @@ export const validProductInput = {
   co2_rating: 'A',
 };
 
+export const validUpdateInput = {
+  name: 'Wrenchy',
+  price: 15,
+  co2_rating: 'B',
+};
+
+export const validPartialUpdate = [
+  {
+    name: 'Name update returns 200',
+    data: { name: 'Wrencher' },
+  },
+  {
+    name: 'Price update returns 200',
+    data: { price: 19.99 },
+  },
+  {
+    name: 'category_id update returns 200',
+    data: { category_id: '01KP2PTF5DN31JX5P3Z8J8JCES' },
+  },
+  {
+    name: 'brand_id update returns 200',
+    data: { brand_id: '01KP2PTEV7SGRMZZH3418D8PMV' },
+  },
+  {
+    name: 'is_location_offer update returns 200',
+    data: { is_location_offer: false },
+  },
+  {
+    name: 'is_rental update returns 200',
+    data: { is_rental: false },
+  },
+  {
+    name: 'product_image_id update returns 200',
+    data: { product_image_id: '01KP2PTF5TH1Y68K1HNDNB82F9' },
+  },
+];
+
+export const invalidProductInput = [
+  {
+    name: 'Invalid name returns 422 and error message',
+    data: { name: 1 },
+    field: 'name',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid price returns 422 and error message',
+    data: { price: 'test' },
+    field: 'price',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid brand_id returns 422 and error message',
+    data: { brand_id: 'test' },
+    field: 'brand_id',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid category_id returns 422 and error message',
+    data: { category_id: 'test' },
+    field: 'category_id',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid product_image_id returns 422 and error message',
+    data: { product_image_id: 'test' },
+    field: 'product_image_id',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid is_location_offer returns 422 and error message',
+    data: { is_location_offer: 'test' },
+    field: 'is_location_offer',
+    message: msg.PROD_REQ_NAME,
+  },
+
+  {
+    name: 'Invalid is_rental returns 422 and error message',
+    data: { is_rental: 'test' },
+    field: 'is_rental',
+    message: msg.PROD_REQ_NAME,
+  },
+];
+
 export const missingProductInput = [
   {
     name: 'Required name returns 422 and error message',
