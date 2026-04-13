9691ce1151edcf5bc72a20710fcaa896afa3c71d
Petromir Kolev <petromirkolev@gmail.com>
2026-04-13 09:39:26 +0300

    test: Add update product with valid data test

--- toolshop/tests/products/products-update.api.spec.ts
+++ toolshop/tests/products/products-update.api.spec.ts
@@ -1,4 +1,26 @@
-// 32. Update product with valid payload succeeds.
-// 33. Partial update product with valid payload succeeds.
-// 34. Update product with invalid payload is rejected.
-// 35. Partial update product with invalid field types is rejected.
+import { test, expect } from '../../fixtures/products';
+import { validUpdateInput } from '../../utils/product-data';
+
+test.describe('Toolshop API - Update product', () => {
+  test('Update product with valid data returns 201 and updated product', async ({
+    productApi,
+    customProduct,
+  }) => {
+    const response = await productApi.update(
+      validUpdateInput,
+      customProduct.id,
+    );
+    expect(response.status()).toBe(200);
+
+    const updatedProductResponse = await productApi.getOne(customProduct.id);
+    const updatedProductBody = await updatedProductResponse.json();
+
+    expect(updatedProductBody.id).toMatch(customProduct.id);
+    expect(updatedProductBody.name).toBe(validUpdateInput.name);
+    expect(updatedProductBody.price).toBe(validUpdateInput.price);
+  });
+
+  test('Partial update product with valid data returns 201 and updated product', async () => {});
+
+  test('Partial update product with invalid data returns 401 and original product', async () => {});
+});
